8c6828230fc6b04720ec4f1ac0c5062321e0525e
Marco <marcoerhardt95@gmail.com>
2021-04-06 16:07:56 +0200

    Add projectList function displaying buttons for each new project created, not linked to anything yet

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,12 @@
 import '../assets/style.css';
 
+const toDoItems = []
+
 const toDoItem = (title, description, date, priority, project) => {
   return { title, description, date, priority, project };
 };
 
-const toDoItems = []
-
 const submitButton = document.getElementById('submitInput')
-
 submitButton.addEventListener('click', () => {
   const title = document.getElementById('titleInput').value;
   const description = document.getElementById('descriptionInput').value;
@@ -21,11 +20,21 @@ submitButton.addEventListener('click', () => {
 
 const projectSubmitButton = document.getElementById('projectSubmitInput')
 const projectOptions = document.getElementById('projectInput')
-
 projectSubmitButton.addEventListener('click', () => {
   const project = document.getElementById('projectTitleInput').value;
   const option = document.createElement('option');
   option.setAttribute('value', project);
   option.textContent = project
-  projectInput.append(option)
+  projectOptions.append(option)
+  createProjectButton(project);
 });
+
+const projectList = document.getElementById('project-list')
+const createProjectButton = (project) => {
+  const projectLink = document.createElement('button');
+  projectLink.setAttribute('data-project', project);
+  projectLink.setAttribute('id', project + 'Link');
+  projectLink.textContent = project
+  projectList.append(projectLink)
+}
+
