176958623ae1254e97b66b3aa89b2fa81b90b09d
Marco <marcoerhardt95@gmail.com>
2021-04-06 15:05:50 +0200

    Add html form for creating to-do-item, same for creating project, add eventlisteners for both and functionality that includes a newly created project in the form for creating items

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,31 @@
 import '../assets/style.css';
 
+const toDoItem = (title, description, date, priority, project) => {
+  return { title, description, date, priority, project };
+};
 
+const toDoItems = []
+
+const submitButton = document.getElementById('submitInput')
+
+submitButton.addEventListener('click', () => {
+  const title = document.getElementById('titleInput').value;
+  const description = document.getElementById('descriptionInput').value;
+  const date = document.getElementById('dateInput').value;
+  const priority = document.getElementById('priorityInput').value;
+  const project = document.getElementById('projectInput').value;
+  const newItem = toDoItem(title, description, date, priority, project);
+  toDoItems.push(newItem);
+  console.log(toDoItems)
+});
+
+const projectSubmitButton = document.getElementById('projectSubmitInput')
+const projectOptions = document.getElementById('projectInput')
+
+projectSubmitButton.addEventListener('click', () => {
+  const project = document.getElementById('projectTitleInput').value;
+  const option = document.createElement('option');
+  option.setAttribute('value', project);
+  option.textContent = project
+  projectInput.append(option)
+});
