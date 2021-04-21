a15404311c23faca0c7b953500befe9527c76d5b
Marco <marcoerhardt95@gmail.com>
2021-04-21 15:24:58 +0200

    Install jest, write tests for append.js

---
+++ tests/append.test.js
@@ -0,0 +1,17 @@
+import appendElements from '../src/components/append';
+
+describe('append dom elements', () => {
+  const arr = [1,2];
+  const div = document.createElement('div');
+
+  test('Creates single todo-item', () => {
+    appendElements(arr, div);
+    expect(div.innerHTML).toContain('single-todo-item');
+  });
+
+  test('Creates unique data-attributes to differentiate between todo-items', () => {
+    appendElements(arr, div);
+    expect(div.innerHTML).toContain('data-id="0"');
+    expect(div.innerHTML).toContain('data-id="1"');
+  });
+});
